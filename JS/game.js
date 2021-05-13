ae8a088a49af52764593b92fd204d7ad93636a71
EliasV <46247153+EliasVal@users.noreply.github.com>
2021-05-13 17:05:11 +0300

    Improved Matchmaking - Added a search game button, along with cancel - Changed Realtime Database rules to make it more secure - Made buttons look nicer

--- JS/game.js
+++ JS/game.js
@@ -1,8 +1,11 @@
 "use strict";
 $("#usrname").html(fbUser.displayName);
+var gameCancelEvent = document.createEvent('Event');
+gameCancelEvent.initEvent('cancelled', true, false);
 function StartGame() {
     let db = firebase.default.database();
     let roomID = getRandomString(6);
+    sessionStorage.setItem("room_id", roomID);
     let freeRoomRef = db.ref('/free_rooms');
     let occupiedRoomRef = db.ref('/occupied_rooms');
     freeRoomRef.once("value", async (snapshot) => {
@@ -13,36 +16,54 @@ function StartGame() {
             let keepAliveInterval = setInterval(() => {
                 freeRoomRef.child(`${roomID}`).update({ age: Date.now().toString() });
             }, 5 * 1000);
+            function cancelled() {
+                clearInterval(keepAliveInterval);
+                freeRoomRef.update(JSON.parse(`{"${roomID}": null}`));
+                sessionStorage.removeItem('roomID');
+            }
+            addEventListener('cancelled', cancelled);
             let occupiedRoomRef = db.ref('/occupied_rooms');
             occupiedRoomRef.child(roomID).on('value', async (data) => {
                 let dt = await data.val();
                 if (dt != null) {
-                    console.log(dt);
+                    removeEventListener('cancelled', cancelled);
                     clearInterval(keepAliveInterval);
+                    freeRoomRef.update(JSON.parse(`{"${roomID}": null}`));
+                    $('#searchGameCommenced').children()[0].innerHTML = 'Opponent found!';
+                    $('#searchGameCommenced').children()[1].style.display = 'none';
                 }
             });
         }
         else {
             if (Math.floor((Date.now() - parseInt(Object.values(snapshotDT)[0]["age"])) / 1000) > 10) {
-                freeRoomRef.update(JSON.parse(`{"${Object.keys(snapshotDT)[0]}": null}`));
                 let strObj = `{"${roomID}": {"p1": "${fbUser.uid}", "age": "${Date.now()}"}}`;
                 await freeRoomRef.update(JSON.parse(strObj));
                 let keepAliveInterval = setInterval(() => {
                     freeRoomRef.child(`${roomID}`).update({ age: Date.now().toString() });
                 }, 30 * 1000);
+                function cancelled() {
+                    clearInterval(keepAliveInterval);
+                    freeRoomRef.update(JSON.parse(`{"${roomID}": null}`));
+                    sessionStorage.removeItem('roomID');
+                }
+                addEventListener('cancelled', cancelled);
                 let occupiedRoomRef = db.ref('/occupied_rooms');
                 occupiedRoomRef.child(roomID).on('value', async (data) => {
                     let dt = await data.val();
                     if (dt != null) {
-                        console.log(dt);
+                        removeEventListener('cancelled', cancelled);
                         clearInterval(keepAliveInterval);
+                        freeRoomRef.update(JSON.parse(`{"${roomID}": null}`));
+                        $('#searchGameCommenced').children()[0].innerHTML = 'Opponent found!';
+                        $('#searchGameCommenced').children()[1].style.display = 'none';
                     }
                 });
             }
             else {
                 let strObj = `{"${Object.keys(snapshotDT)[0]}": {"participants": {"${fbUser.uid}": {"pts": 0}, "${Object.values(snapshotDT)[0]["p1"]}": {"pts": 0}}}}`;
                 occupiedRoomRef.update(JSON.parse(strObj));
-                freeRoomRef.update(JSON.parse(`{"${Object.keys(snapshotDT)[0]}": null}`));
+                $('#searchGameCommenced').children()[0].innerHTML = 'Opponent found!';
+                $('#searchGameCommenced').children()[1].style.display = 'none';
             }
         }
     });
